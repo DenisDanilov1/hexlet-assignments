162b8af2a4bce65333b15434dd5191fee09f1bba
@hexlet/cli <support@hexlet.io>
2024-06-27 17:57:18 +0400

    submit java-oop-ru/classes-and-objects

--- java-oop-ru/classes-and-objects/src/main/java/exercise/Segment.java
+++ java-oop-ru/classes-and-objects/src/main/java/exercise/Segment.java
@@ -1,22 +1,28 @@
 package exercise;
 
 // BEGIN
-import lombok.Getter;
+class Segment {
 
-@Getter
-public class Segment {
-     private Point beginPoint;
-     private Point endPoint;
+    private Point beginPoint;
+    private Point endPoint;
 
-     public Segment(){}
+    public Segment(Point beginPoint, Point endPoint) {
+        this.beginPoint = beginPoint;
+        this.endPoint = endPoint;
+    }
 
-     public Segment(Point beginPoint, Point endPoint) {
-         this.beginPoint = beginPoint;
-         this.endPoint = endPoint;
-     }
-     public Point getMidPoint() {
-	 return new Point((beginPoint.getX() + endPoint.getX()) / 2,
-                        (beginPoint.getY() + endPoint.getY()) / 2 );
-     }
+    public Point getBeginPoint() {
+        return beginPoint;
+    }
+
+    public Point getEndPoint() {
+        return endPoint;
+    }
+
+    public Point getMidPoint() {
+        int midX = (beginPoint.getX() + endPoint.getX()) / 2;
+        int midY = (beginPoint.getY() + endPoint.getY()) / 2;
+        return new Point(midX, midY);
+    }
 }
 // END
