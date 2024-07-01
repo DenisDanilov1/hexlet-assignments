b3381a993916f6dbe6a255c8b7e512cfb576e4f9
@hexlet/cli <support@hexlet.io>
2024-07-01 17:51:41 +0400

    submit java-oop-ru/errors

--- java-oop-ru/errors/src/main/java/exercise/App.java
+++ java-oop-ru/errors/src/main/java/exercise/App.java
@@ -6,11 +6,11 @@ public class App {
     public static void printSquare(Circle circle) {
         try {
             int square = (int) Math.ceil(circle.getSquare());
-             System.out.println(square);
+            System.out.println(square);
         } catch (NegativeRadiusException e) {
             System.out.println("Не удалось посчитать площадь");
         }
-            System.out.println("Вычисление окончено");
+        System.out.println("Вычисление окончено");
     }
 }
 // END
