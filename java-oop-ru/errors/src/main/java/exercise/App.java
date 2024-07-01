12890ff57c6b491a1a1db6c2530e767b560a235d
@hexlet/cli <support@hexlet.io>
2024-07-01 14:29:19 +0400

    submit java-oop-ru/errors

--- java-oop-ru/errors/src/main/java/exercise/App.java
+++ java-oop-ru/errors/src/main/java/exercise/App.java
@@ -5,8 +5,8 @@ public class App {
 
     public static void printSquare(Circle circle) {
         try {
-            int square = (int) Math.round(Math.PI * circle.getRadius() * circle.getRadius());
-            throw new NegativeRadiusException(toString(square));
+            int square = (int) Math.round(Math.PI * Math.pow(circle.getRadius(), 2));
+            System.out.println("The area of the circle: " + square);
         } catch (NegativeRadiusException e) {
             System.out.println("The area could not be calculated");
         } finally {
