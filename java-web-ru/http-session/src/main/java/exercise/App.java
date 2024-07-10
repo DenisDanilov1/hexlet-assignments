65bdfde444223f1d592c16283758e0fed58ec493
@hexlet/cli <support@hexlet.io>
2024-07-10 13:37:19 +0400

    submit java-web-ru/http-session

--- java-web-ru/http-session/src/main/java/exercise/App.java
+++ java-web-ru/http-session/src/main/java/exercise/App.java
@@ -16,9 +16,9 @@ public final class App {
 
         // BEGIN
         app.get("/users", ctx -> {
-        var page = ctx.queryParam("page"); });
+        var page = ctx.bodyAsClass(clazz); });
 	app.get("/users?page=5&per=3", ctx -> {
-        var page = ctx.queryParam("page"); });
+        var page = ctx.bodyAsClass(clazz); });
         // END
         return app;
 
