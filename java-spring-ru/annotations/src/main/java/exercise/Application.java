01b0b06560dc743dd2a339ed025c669278710be3
@hexlet/cli <support@hexlet.io>
2024-07-29 17:00:36 +0400

    submit java-spring-ru/annotations

--- java-spring-ru/annotations/src/main/java/exercise/Application.java
+++ java-spring-ru/annotations/src/main/java/exercise/Application.java
@@ -10,7 +10,7 @@ public class Application {
 
         // BEGIN
         for (Method method : Address.class.getDeclaredMethods()) {
-            if (method.isAnnotationPresent(exercise.annotation.Inspect.class)) {
+            if (method.isAnnotationPresent(Inspect.class)) {
                 System.out.println("Method " + method.getName() + " returns a value of type " + method.getReturnType());
             }
         }
