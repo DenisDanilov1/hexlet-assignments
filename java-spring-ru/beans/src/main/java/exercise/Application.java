98acee5c0e15cf64ce9840341cfc9be253d36741
@hexlet/cli <support@hexlet.io>
2024-10-04 16:44:19 +0400

    submit java-spring-ru/beans

--- java-spring-ru/beans/src/main/java/exercise/Application.java
+++ java-spring-ru/beans/src/main/java/exercise/Application.java
@@ -10,7 +10,8 @@ import exercise.daytime.Day;
 import exercise.daytime.Night;
 
 // BEGIN
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.annotation.RequestScope;
 // END
 
 @SpringBootApplication
@@ -21,6 +22,16 @@ public class Application {
     }
 
     // BEGIN
-    
+    @RequestScope
+    @Bean
+    public Daytime getDayOrNight() {
+        var dateTime = LocalDateTime.now();
+        var hour = dateTime.getHour();
+        if (hour >= 6 & hour <= 22) {
+            return new Day();
+        } else {
+            return new Night();
+        }
+    }
     // END
 }
