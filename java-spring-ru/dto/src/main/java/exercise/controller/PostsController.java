e31d7497feba265197f9846204c9645933ccd084
@hexlet/cli <support@hexlet.io>
2024-10-05 18:19:04 +0400

    submit java-spring-ru/dto

--- java-spring-ru/dto/src/main/java/exercise/controller/PostsController.java
+++ java-spring-ru/dto/src/main/java/exercise/controller/PostsController.java
@@ -13,6 +13,7 @@ import exercise.repository.PostRepository;
 import exercise.exception.ResourceNotFoundException;
 import exercise.dto.PostDTO;
 import exercise.dto.CommentDTO;
+import exercise.model.Comment;
 
 @RestController
 @RequestMapping("posts")
