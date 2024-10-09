492b8227ce7446c01a9a192f67a4986553ed62bf
@hexlet/cli <support@hexlet.io>
2024-10-09 13:05:35 +0400

    submit java-spring-ru/crud

--- java-spring-ru/crud/src/main/java/exercise/controller/ProductsController.java
+++ java-spring-ru/crud/src/main/java/exercise/controller/ProductsController.java
@@ -17,7 +17,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import exercise.repository.CategoryRepository;
 import exercise.exception.ResourceNotFoundException;
 import exercise.repository.ProductRepository;
 import jakarta.validation.Valid;
