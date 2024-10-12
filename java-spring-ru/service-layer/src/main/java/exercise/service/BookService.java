f300262fe666738326238b0ae9328e414deae8b7
@hexlet/cli <support@hexlet.io>
2024-10-12 21:04:36 +0400

    submit java-spring-ru/service-layer

--- java-spring-ru/service-layer/src/main/java/exercise/service/BookService.java
+++ java-spring-ru/service-layer/src/main/java/exercise/service/BookService.java
@@ -9,6 +9,8 @@ import exercise.repository.BookRepository;
 import exercise.repository.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
 
 import java.util.List;
 
