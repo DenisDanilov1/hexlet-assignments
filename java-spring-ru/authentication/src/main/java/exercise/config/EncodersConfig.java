4db985aa77c919c5f6884e42ba1daafff7bc2d4a
DenisDanilov1 <denchik199819981998@gmail.com>
2024-10-14 14:33:24 +0400

    added changes to the task

--- java-spring-ru/authentication/src/main/java/exercise/config/EncodersConfig.java
+++ java-spring-ru/authentication/src/main/java/exercise/config/EncodersConfig.java
@@ -7,7 +7,6 @@ import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 import exercise.component.RsaKeyProperties;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -38,7 +37,6 @@ public class EncodersConfig {
     }
 
     @Bean
-    @NotNull
     JwtDecoder jwtDecoder() {
         return NimbusJwtDecoder.withPublicKey(rsaKeys.getPublicKey()).build();
     }
